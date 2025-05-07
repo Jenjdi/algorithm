9d87d277f13fd9d0a46f73536762a2693ecf1105
package <2778120659@qq.com>
2025-05-07 11:01:26 +0800

    delete all files

--- Clanguage Quiz/head.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-#include<stdio.h>
-#include<assert.h>
-#include<stdlib.h>
-void q2();
--- Clanguage Quiz/question2.c
+++
@@ -1,9 +0,0 @@
-#include"head.h"
-void q2()
-{
-	int value = 1024;
-	char condition = *((char*)(&value));
-	if (condition) value += 1; condition = *((char*)(&value));
-	if (condition) value += 1; condition = *((char*)(&value));
-	printf("%d %d", value, condition);
-}
