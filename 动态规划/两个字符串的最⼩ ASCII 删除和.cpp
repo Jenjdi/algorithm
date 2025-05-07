9989c007877ab6394412dfd60e975d5aa48b7b27
package <2778120659@qq.com>
2025-05-07 15:19:19 +0800

    两个字符串的最⼩ ASCII 删除和

--- 动态规划/两个字符串的最⼩ ASCII 删除和.cpp
+++ 动态规划/两个字符串的最⼩ ASCII 删除和.cpp
@@ -21,6 +21,25 @@ using namespace std;
 class Solution {
     public:
         int minimumDeleteSum(string s1, string s2) {
-            
+            int m=s1.size();
+            int n=s2.size();
+            vector<vector<int>> dp(m+1,vector<int>(n+1,0));
+            for(int i=1;i<=m;i++)
+            {
+                for(int j=1;j<=n;j++)
+                {
+                    dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+                    if(s1[i-1]==s2[j-1])
+                    {
+                        dp[i][j]=max(dp[i][j],dp[i-1][j-1]+s1[i-1]);
+                    }
+                }
+            }
+            int sum=0;
+            for(auto e:s1)
+                sum+=e;
+            for(auto e:s2)
+                sum+=e;
+            return sum-2*dp[m][n];
         }
     };
